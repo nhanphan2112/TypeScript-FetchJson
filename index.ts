05048559ffdbe91aee1e39e42d8c54e3bc4258b5
Nhan Phan <nphanacc@gmail.com>
2021-07-16 10:28:47 -0700

    325-Catching error by adding type to variable name inside function definition

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { timeLog } from "node:console";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
@@ -15,9 +16,15 @@ axios.get(url).then((response) => {
 	const title = todo.title;
 	const completed = todo.completed;
 
+  logTodo(id, title, completed);
+
+});
+
+const logTodo = (id:number, title:string, completed:boolean) => {
 	console.log(`
     The Todo with ID: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+
+};
